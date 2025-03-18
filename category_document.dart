0037f0cad39c0bdb4c43cc16d083f7f156a56725
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-18 13:41:04 +0100

    class contractpartners added.

--- category_document.dart
+++ category_document.dart
@@ -1,3 +1,11 @@
 // es gibt eine Liste mit vorgeschlagenen oder inkl. hinzugefügten Kategorien
-// und es gibt die Möglichkeit, eine neue Kategorie zu erstellen, 
+// und es gibt die Möglichkeit, eine neue Kategorie zu erstellen,
 // die dann automatisch in der Liste aller Kategoireien ist zukünftig
+
+class CategoryDocument {
+  //Attribute
+  List<String> docCategory = [];
+  String newCategory;
+
+  CategoryDocument(this.docCategory, this.newCategory);
+}
--- contractpartner_list.dart
+++ contractpartner_list.dart
@@ -1,3 +1,13 @@
 // es gibt eine Liste mit vorgeschlagenen oder inkl. hinzugefügten Vertragspartnern
-// und es gibt die Möglichkeit, eine neuen Vertragspartner anzulegen, 
+// und es gibt die Möglichkeit, eine neuen Vertragspartner anzulegen,
 // die dann automatisch in der Liste aller vorhandenen Vertragspartner ist zukünftig
+
+import 'contracptpartner_profile.dart';
+
+class Contractpartners {
+  //Attribute
+  List<String> contPartners = [];
+  var ContracptpartnerProfile;
+
+  Contractpartners(this.contPartners, this.ContracptpartnerProfile);
+}
