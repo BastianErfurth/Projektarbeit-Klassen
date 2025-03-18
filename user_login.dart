d69aca6ce9c6e7f01850af566413e4d1ec2a5a8a
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-18 11:27:02 +0100

    start ith class Registration and Log in

---
+++ user_login.dart
@@ -0,0 +1,13 @@
+class UserLogin {
+  //Attribute
+
+  String email;
+  String password;
+  bool hasClickedLogin;
+  bool isLoginSuccessful;
+
+
+  UserLogin(this.email, this.password,this.hasClickedLogin,this isLoginSuccessful);
+}
+
+
---
+++ user_registration.dart
@@ -0,0 +1,13 @@
+class UserRegistration {
+  //Attribute
+  String lastName;
+  String firstName;
+  String email;
+  String password;
+  String securityQuestion;
+  String securityAnswer;
+
+  //Konstruktor
+  UserRegistration(this.lastName, this.firstName, this.email, this.password,
+      this.securityQuestion, this.securityAnswer);
+}
