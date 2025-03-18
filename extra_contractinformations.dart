e66aae7635c4bde48a21a393d26cc6124ef6c33f
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-18 14:58:02 +0100

    extra contractinformations added

---
+++ extra_contractinformations.dart
@@ -0,0 +1,7 @@
+class ExtraContractinformations {
+  String ownRemarks;
+  String generalMissingRemarks;
+
+  //Konstruktor
+  ExtraContractinformations(this.ownRemarks, this.generalMissingRemarks);
+}
